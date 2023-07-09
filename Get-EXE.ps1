05555503034d0d7c35d09a2d29d49606be22732b
Salman-Kb <137834927+Salman-Kb@users.noreply.github.com>
2023-07-09 13:10:53 +0400

    Add files via upload

---
+++ Get-EXE.ps1
@@ -0,0 +1,18 @@
+﻿cd C:\users
+ Get-ChildItem -Filter "*.exe" -Recurse | Select Name, Directory, Length
+
+$CheckD = 'D:\'
+if (Test-Path -Path $CheckD) {
+    cd D:\
+    Get-ChildItem -Filter "*.exe" -Recurse | Select Name, Directory, Length
+} else {
+    Write-Host "Does not exist"
+}
+
+$CheckE = 'E:\'
+if (Test-Path -Path $CheckE) {
+    cd E:\
+     Get-ChildItem -Filter "*.exe" -Recurse | Select Name, Directory, Length
+} else {
+    Write-Host "Does not exist"
+}
